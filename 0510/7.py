703c8a7b15cd3681404fdf6b2740f84ce03c7d88
RudiFrost <rudifrost255@gmail.com>
2021-10-05 14:35:28 +0300

    0510

--- 0510/7.py
+++ 0510/7.py
@@ -1,14 +1,12 @@
 import random
 
 
-def rand(a, b, c):
-    while True:
+def rand(a, b, c, d):
+    i = 0
+    while i < d:
         yield random.choice(a) + " " + random.choice(b) + " " + random.choice(c)
+        i += 1
 
 
-q = 0
-for i in rand(["asd", "qwe", "zxc", "rty"], ["123", "345", "567", "987"], ["zzz", "xxx", "ccc", "qqq"]):
-    if q > 20:
-        break
+for i in rand(["asd", "qwe", "zxc", "rty"], ["123", "345", "567", "987"], ["zzz", "xxx", "ccc", "qqq"], 20):
     print(i)
-    q += 1
